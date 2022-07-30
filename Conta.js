b16d5b19ec52ef7bcaab5b513a064ec0ac27cead
charlesgabriel <charlesgabriel@hotmail.com.br>
2022-07-30 16:10:15 -0300

    trabalhado com método abstrato/worked with abstract method

--- Conta.js
+++ Conta.js
@@ -2,7 +2,7 @@
 export class Conta {
     constructor(cliente, agencia, saldoInicial) {
         if(this.constructor == Conta) {
-            throw new Error("Você não deveria instanciar um objeto do tipo Conta diretamente!");
+            throw new Error("Você não deveria instanciar um objeto do tipo Conta diretamente, pois essa é uma classe abstrata!");
         }
         this._agencia = agencia;
         this._cliente = cliente;
@@ -23,9 +23,9 @@ export class Conta {
         return this._saldo;
     }
 
+    // Método abstrato
     sacar(valor) {
-        let taxa = 1;
-        return this._sacar(valor, taxa);
+        throw new Error("O método Sacar da conta é abstrato!");
     }
 
     _sacar(valor, taxa) {
