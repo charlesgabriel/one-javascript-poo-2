6d78eea564f36c2bdeba9b77309976c47fcf9b05
charlesgabriel <charlesgabriel@hotmail.com.br>
2022-07-30 14:53:48 -0300

    trabalhado com propriedade constructor/worked with constructor property

--- Conta.js
+++ Conta.js
@@ -3,6 +3,9 @@ export class Conta {
         this._agencia = agencia;
         this._cliente = cliente;
         this._saldo = saldoInicial;
+        if(this.constructor == Conta) {
+            console.log("Você não deveria instanciar um objeto do tipo conta!");
+        }
     }
 
     set cliente(novoValor) {
