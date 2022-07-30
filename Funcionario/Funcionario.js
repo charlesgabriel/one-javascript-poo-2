a731d69580d38d52d036915456d9c89085cf28c8
charlesgabriel <charlesgabriel@hotmail.com.br>
2022-07-30 18:41:56 -0300

    criado a seção de funcionários/created the employees section

---
+++ Funcionario/Funcionario.js
@@ -0,0 +1,8 @@
+export class Funcionario {
+    constructor(nome, cpf, salario) {
+        this._nome = nome;
+        this._cpf = cpf;
+        this._salario = salario;
+        this._bonificacao = 1;
+    }
+}
